2076ddbda1720ae2a9ce62fbe4c7e2b1a679c908
Morkinali <kikiani.nika@gmail.com>
2024-02-17 00:36:26 +0400

    Artua 17

---
+++ source/repos/Artur/Artur/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Artur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Tom");
+        }
+
+    }
+}
